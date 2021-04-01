a37a9d16f5b9d05dbd07b46553195e3bf8b7076b
SchnitzelB <es971120@icloud.com>
2021-04-01 08:47:40 -0500

    Initial commit for Contact.java

--- Contact.java
+++ Contact.java
@@ -1,3 +1,27 @@
 public class Contact {
-  
+
+  private static String name;
+  private static String phoneNumber;
+
+  public Contact(String name, String phoneNumber) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+  }
+
+  //getName() - getter for your name
+  public String getName() {
+    return this.name;
+  }
+
+  //getPhoneNumber() - getter for phoneNumber;
+  public String getPhoneNumber() {
+    return this.phoneNumber;
+  }
+
+  //STATIC createContact(String name, String phoneNumber) - returns instance of Contact
+  public static ArrayList<Contact> createContact(String name, String phoneNumber) {
+    
+  }
+
+
 }
